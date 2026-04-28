64e138bc54ae93cdde0936595c9c24e1c6c0806c
Melroy van den Berg <melroy@melroy.org>
2026-04-28 21:39:18 +0200

    Merge pull request #1 from BitcoinCash1/move_to_ts
    Move to ts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,38 @@
-'use strict';
-
-const Client = require('./lib/client');
+import { Client } from './lib/client.js';
+import { Callbacks, ElectrumConfig, PersistencePolicy } from './types/index.js';
 
 class ElectrumClient extends Client {
-	constructor(port, host, protocol, options, callbacks) {
-		super(port, host, protocol, options, callbacks);
+	private onConnectCallback: ((client: ElectrumClient, versionInfo: [string, string]) => void) | null;
+	private onCloseCallback: ((client: ElectrumClient) => void) | null;
+	private onLogCallback: (str: string) => void;
+	private timeLastCall: number;
+	private persistencePolicy: PersistencePolicy | null = null;
+	private electrumConfig: ElectrumConfig | null = null;
+	private timeout: NodeJS.Timeout | null;
+	public versionInfo: [string, string];
+
+	constructor(port: number, host: string, protocol: string, callbacks?: Callbacks) {
+		super(port, host, protocol, callbacks);
 
 		this.onConnectCallback = (callbacks && callbacks.onConnect) ? callbacks.onConnect : null;
 		this.onCloseCallback = (callbacks && callbacks.onClose) ? callbacks.onClose : null;
-		this.onLogCallback = (callbacks && callbacks.onLog) ? callbacks.onLog : function(str) {
+		this.onLogCallback = (callbacks && callbacks.onLog) ? callbacks.onLog : function(str: string) {
 			console.log(str);
 		};
 
 		this.timeLastCall = 0;
+		this.timeout = null;
+		this.versionInfo = ['', ''];
 	}
 
-	initElectrum(electrumConfig, persistencePolicy = { retryPeriod: 10000, maxRetry: 1000, pingPeriod: 120000, callback: null }) {
+	initElectrum(electrumConfig: ElectrumConfig, persistencePolicy: PersistencePolicy = { retryPeriod: 10000, maxRetry: 1000, pingPeriod: 120000, callback: null }): Promise<ElectrumClient> {
 		this.persistencePolicy = persistencePolicy;
 		this.electrumConfig = electrumConfig;
 		this.timeLastCall = 0;
 
 		return new Promise((resolve, reject) => {
 			this.connect().then(() => {
-				this.server_version(this.electrumConfig.client, this.electrumConfig.version).then((versionInfo) => {
+				this.server_version(electrumConfig.client, electrumConfig.version).then((versionInfo) => {
 					this.versionInfo = versionInfo;
 
 					if (this.onConnectCallback != null) {
@@ -41,7 +51,7 @@ class ElectrumClient extends Client {
 	}
 
 	// Override parent
-	request(method, params) {
+	request(method: string, params: any[]): Promise<any> {
 		this.timeLastCall = new Date().getTime();
 
 		const parentPromise = super.request(method, params);
@@ -53,7 +63,7 @@ class ElectrumClient extends Client {
 		});
 	}
 
-	requestBatch(method, params, secondParam) {
+	requestBatch(method: string, params: any[], secondParam?: any): Promise<any> {
 		this.timeLastCall = new Date().getTime();
 
 		const parentPromise = super.requestBatch(method, params, secondParam);
@@ -65,7 +75,7 @@ class ElectrumClient extends Client {
 		});
 	}
 
-	onClose() {
+	onClose(): void {
 		super.onClose();
 
 		const list = [
@@ -77,8 +87,8 @@ class ElectrumClient extends Client {
 
 		list.forEach(event => this.subscribe.removeAllListeners(event));
 
-		var retryPeriod = 10000;
-		if (this.persistencePolicy != null && this.persistencePolicy.retryPeriod > 0) {
+		let retryPeriod = 10000;
+		if (this.persistencePolicy?.retryPeriod && this.persistencePolicy.retryPeriod > 0) {
 			retryPeriod = this.persistencePolicy.retryPeriod;
 		}
 
@@ -87,17 +97,17 @@ class ElectrumClient extends Client {
 		}
 
 		setTimeout(() => {
-			if (this.persistencePolicy != null && this.persistencePolicy.maxRetry > 0) {
+				if (this.persistencePolicy?.maxRetry && this.persistencePolicy.maxRetry > 0) {
 				this.reconnect().catch((err) => {
 					this.onError(err);
 				});
 
 				this.persistencePolicy.maxRetry -= 1;
 
-			} else if (this.persistencePolicy != null && this.persistencePolicy.callback != null) {
+			} else if (this.persistencePolicy?.callback) {
 				this.persistencePolicy.callback();
 
-			} else if (this.persistencePolicy == null) {
+			} else if (!this.persistencePolicy) {
 				this.reconnect().catch((err) => {
 					this.onError(err);
 				});
@@ -106,161 +116,163 @@ class ElectrumClient extends Client {
 	}
 
 	// ElectrumX persistancy
-	keepAlive() {
+	keepAlive(): void {
 		if (this.timeout != null) {
 			clearTimeout(this.timeout);
 		}
 
-		var pingPeriod = 120000;
-		if (this.persistencePolicy != null && this.persistencePolicy.pingPeriod > 0) {
+		let pingPeriod = 120000;
+		if (this.persistencePolicy?.pingPeriod && this.persistencePolicy.pingPeriod > 0) {
 			pingPeriod = this.persistencePolicy.pingPeriod;
 		}
 
 		this.timeout = setTimeout(() => {
 			if (this.timeLastCall !== 0 && new Date().getTime() > this.timeLastCall + pingPeriod) {
 				this.server_ping().catch((reason) => {
-					this.log('Keep-Alive ping failed: ', reason);
+					this.log('Keep-Alive ping failed: ' + reason);
 				});
 			}
 		}, pingPeriod);
 	}
 
-	close() {
+	close(): void {
 		super.close();
 
 		if (this.timeout != null) {
 			clearTimeout(this.timeout);
 		}
 
-		this.reconnect = this.reconnect = this.onClose = this.keepAlive = () => {}; // dirty hack to make it stop reconnecting
+		this.reconnect = () => Promise.resolve(this); // dirty hack to make it stop reconnecting
+		this.onClose = () => Promise.resolve(this); // dirty hack to make it stop reconnecting
+		this.keepAlive = () => Promise.resolve(this); // dirty hack to make it stop reconnecting
 	}
 
-	reconnect() {
+	reconnect(): Promise<ElectrumClient> {
 		this.log("Electrum attempting reconnect...");
 		
 		this.initSocket();
 
-		if (this.persistencePolicy != null) {
-			return this.initElectrum(this.electrumConfig, this.persistencePolicy);
+		if (this.persistencePolicy) {
+			return this.initElectrum(this.electrumConfig!, this.persistencePolicy!);
 			
 		} else {
-			return this.initElectrum(this.electrumConfig);
+			return this.initElectrum(this.electrumConfig!);
 		}
 	}
 
-	log(str) {
+	log(str: string): void {
 		this.onLogCallback(str);
 	}
 
 	// ElectrumX API
-	server_version(client_name, protocol_version) {
+	server_version(client_name: string, protocol_version: string | [string, string]): Promise<any> {
 		return this.request('server.version', [client_name, protocol_version]);
 	}
-	server_banner() {
+	server_banner(): Promise<any> {
 		return this.request('server.banner', []);
 	}
-	server_features() {
+	server_features(): Promise<any> {
 		return this.request('server.features', []);
 	}
-	server_ping() {
+	server_ping(): Promise<any> {
 		return this.request('server.ping', []);
 	}
-	server_addPeer(features) {
+	server_addPeer(features: any): Promise<any> {
 		return this.request('server.add_peer', [features]);
 	}
-	serverDonation_address() {
+	serverDonation_address(): Promise<any> {
 		return this.request('server.donation_address', []);
 	}
-	serverPeers_subscribe() {
+	serverPeers_subscribe(): Promise<any> {
 		return this.request('server.peers.subscribe', []);
 	}
-	blockchainAddress_getProof(address) {
+	blockchainAddress_getProof(address: string): Promise<any> {
 		return this.request('blockchain.address.get_proof', [address]);
 	}
-	blockchainScripthash_getBalance(scripthash) {
+	blockchainScripthash_getBalance(scripthash: string): Promise<any> {
 		return this.request('blockchain.scripthash.get_balance', [scripthash]);
 	}
-	blockchainScripthash_getBalanceBatch(scripthash) {
+	blockchainScripthash_getBalanceBatch(scripthash: string[]): Promise<any> {
 		return this.requestBatch('blockchain.scripthash.get_balance', scripthash);
 	}
-	blockchainScripthash_listunspentBatch(scripthash) {
+	blockchainScripthash_listunspentBatch(scripthash: string[]): Promise<any> {
 		return this.requestBatch('blockchain.scripthash.listunspent', scripthash);
 	}
-	blockchainScripthash_getHistory(scripthash) {
+	blockchainScripthash_getHistory(scripthash: string): Promise<any> {
 		return this.request('blockchain.scripthash.get_history', [scripthash]);
 	}
-	blockchainScripthash_getHistoryBatch(scripthash) {
+	blockchainScripthash_getHistoryBatch(scripthash: string[]): Promise<any> {
 		return this.requestBatch('blockchain.scripthash.get_history', scripthash);
 	}
-	blockchainScripthash_getMempool(scripthash) {
+	blockchainScripthash_getMempool(scripthash: string): Promise<any> {
 		return this.request('blockchain.scripthash.get_mempool', [scripthash]);
 	}
-	blockchainScripthash_listunspent(scripthash) {
+	blockchainScripthash_listunspent(scripthash: string): Promise<any> {
 		return this.request('blockchain.scripthash.listunspent', [scripthash]);
 	}
-	blockchainScripthash_subscribe(scripthash) {
+	blockchainScripthash_subscribe(scripthash: string): Promise<any> {
 		return this.request('blockchain.scripthash.subscribe', [scripthash]);
 	}
-	blockchainBlock_getHeader(height) {
+	blockchainBlock_getHeader(height: number): Promise<any> {
 		return this.request('blockchain.block.get_header', [height]);
 	}
-	blockchainBlock_headers(start_height, count) {
-		return this.request('blockchain.block.headeres', [start_height, count]);
+	blockchainBlock_headers(start_height: number, count: number): Promise<any> {
+		return this.request('blockchain.block.headers', [start_height, count]);
 	}
-	blockchainEstimatefee(number) {
+	blockchainEstimatefee(number: number): Promise<any> {
 		return this.request('blockchain.estimatefee', [number]);
 	}
-	blockchainHeaders_subscribe(raw) {
+	blockchainHeaders_subscribe(raw?: boolean): Promise<any> {
 		return this.request('blockchain.headers.subscribe', [raw || false]);
 	}
-	blockchain_relayfee() {
+	blockchain_relayfee(): Promise<any> {
 		return this.request('blockchain.relayfee', []);
 	}
-	blockchainTransaction_broadcast(rawtx) {
+	blockchainTransaction_broadcast(rawtx: string): Promise<any> {
 		return this.request('blockchain.transaction.broadcast', [rawtx]);
 	}
-	blockchainTransaction_get(tx_hash, verbose) {
+	blockchainTransaction_get(tx_hash: string, verbose?: boolean): Promise<any> {
 		return this.request('blockchain.transaction.get', [tx_hash, verbose || false]);
 	}
-	blockchainTransaction_getBatch(tx_hash, verbose) {
+	blockchainTransaction_getBatch(tx_hash: string[], verbose?: boolean): Promise<any> {
 		return this.requestBatch('blockchain.transaction.get', tx_hash, verbose);
 	}
-	blockchainTransaction_getMerkle(tx_hash, height) {
+	blockchainTransaction_getMerkle(tx_hash: string, height: number): Promise<any> {
 		return this.request('blockchain.transaction.get_merkle', [tx_hash, height]);
 	}
-	mempool_getFeeHistogram() {
+	mempool_getFeeHistogram(): Promise<any> {
 		return this.request('mempool.get_fee_histogram', []);
 	}
 	// ---------------------------------
 	// protocol 1.1 deprecated method
 	// ---------------------------------
-	blockchainUtxo_getAddress(tx_hash, index) {
+	blockchainUtxo_getAddress(tx_hash: string, index: number): Promise<any> {
 		return this.request('blockchain.utxo.get_address', [tx_hash, index]);
 	}
-	blockchainNumblocks_subscribe() {
+	blockchainNumblocks_subscribe(): Promise<any> {
 		return this.request('blockchain.numblocks.subscribe', []);
 	}
 	// ---------------------------------
 	// protocol 1.2 deprecated method
 	// ---------------------------------
-	blockchainBlock_getChunk(index) {
+	blockchainBlock_getChunk(index: number): Promise<any> {
 		return this.request('blockchain.block.get_chunk', [index]);
 	}
-	blockchainAddress_getBalance(address) {
+	blockchainAddress_getBalance(address: string): Promise<any> {
 		return this.request('blockchain.address.get_balance', [address]);
 	}
-	blockchainAddress_getHistory(address) {
+	blockchainAddress_getHistory(address: string): Promise<any> {
 		return this.request('blockchain.address.get_history', [address]);
 	}
-	blockchainAddress_getMempool(address) {
+	blockchainAddress_getMempool(address: string): Promise<any> {
 		return this.request('blockchain.address.get_mempool', [address]);
 	}
-	blockchainAddress_listunspent(address) {
+	blockchainAddress_listunspent(address: string): Promise<any> {
 		return this.request('blockchain.address.listunspent', [address]);
 	}
-	blockchainAddress_subscribe(address) {
+	blockchainAddress_subscribe(address: string): Promise<any> {
 		return this.request('blockchain.address.subscribe', [address]);
 	}
 }
 
-module.exports = ElectrumClient;
+export { ElectrumClient };
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,34 +1,45 @@
-'use strict';
-let net = require('net');
-let tls = require('tls');
+import net from 'net';
+import tls from 'tls';
+import { EventEmitter } from 'events';
+import { TlsSocketWrapper } from './tls-socket-wrapper.js';
+import * as util from './util.js';
+import { Callbacks, ElectrumRequestBatchParams, ElectrumRequestParams } from '../types/index.js';
+
 const TIMEOUT = 60000;
 
-const TlsSocketWrapper = require('./TlsSocketWrapper.js');
-const EventEmitter = require('events').EventEmitter;
-const util = require('./util');
+export abstract class Client {
 
-class Client {
+	protected id: number;
+	protected port: number;
+	protected host: string;
+	protected callback_message_queue: Record<number, (err: Error | null, result?: any) => void>;
+	protected subscribe: EventEmitter;
+	protected mp: util.MessageParser;
+	protected conn: net.Socket | TlsSocketWrapper | null;
+	protected status: number;
+	private _protocol: string;
+	protected onErrorCallback: ((e: Error) => void) | null;
 
-	constructor(port, host, protocol, options, callbacks) {
+	constructor(port: number, host: string, protocol: string, callbacks?: Callbacks) {
 		this.id = 0;
 		this.port = port;
 		this.host = host;
 		this.callback_message_queue = {};
 		this.subscribe = new EventEmitter();
-		this.mp = new util.MessageParser((body, n) => {
+		this.mp = new util.MessageParser((body: string | undefined, n: number) => {
 			this.onMessage(body, n);
 		});
 		this._protocol = protocol; // saving defaults
-		this._options = options;
+		this.conn = null;
+		this.status = 0;
 
 		this.onErrorCallback = (callbacks && callbacks.onError) ? callbacks.onError : null;
 
-		this.initSocket(protocol, options);
+		this.initSocket(protocol);
 	}
 
-	initSocket(protocol, options) {
+	protected initSocket(protocol?: string): void {
 		protocol = protocol || this._protocol;
-		options = options || this._options;
 		switch (protocol) {
 			case 'tcp':
 				this.conn = new net.Socket();
@@ -46,38 +57,40 @@ class Client {
 				throw new Error('unknown protocol');
 		}
 
-		this.conn.setTimeout(TIMEOUT);
-		this.conn.setEncoding('utf8');
-		this.conn.setKeepAlive(true, 0);
-		this.conn.setNoDelay(true);
-		this.conn.on('connect', () => {
-			this.conn.setTimeout(0);
-			this.onConnect();
-		});
-		this.conn.on('close', e => {
-			this.onClose(e);
-		});
-		this.conn.on('data', chunk => {
-			this.conn.setTimeout(0);
-			this.onRecv(chunk);
-		});
-		this.conn.on('error', e => {
-			this.onError(e);
-		});
+		if (this.conn) {
+			this.conn.setTimeout(TIMEOUT);
+			this.conn.setEncoding('utf8');
+			this.conn.setKeepAlive(true, 0);
+			this.conn.setNoDelay(true);
+			this.conn.on('connect', () => {
+				this.conn && this.conn.setTimeout(0);
+				this.onConnect();
+			});
+			this.conn.on('close', () => {
+				this.onClose();
+			});
+			this.conn.on('data', (chunk: Buffer) => {
+				this.conn && this.conn.setTimeout(0);
+				this.onRecv(chunk);
+			});
+			this.conn.on('error', (e: Error) => {
+				this.onError(e);
+			});
+		}
 		this.status = 0;
 	}
 
-	connect() {
+	protected connect(): Promise<void> {
 		if (this.status === 1) {
 			return Promise.resolve();
 		}
 		this.status = 1;
-		return this.connectSocket(this.conn, this.port, this.host);
+		return this.connectSocket(this.conn!, this.port, this.host);
 	}
 
-	connectSocket(conn, port, host) {
+	private connectSocket(conn: net.Socket | TlsSocketWrapper, port: number, host: string): Promise<void> {
 		return new Promise((resolve, reject) => {
-			const errorHandler = e => reject(e);
+			const errorHandler = (e: Error) => reject(e);
 			
 			conn.on('error', errorHandler);
 
@@ -89,16 +102,16 @@ class Client {
 		});
 	}
 
-	close() {
+	close(): void {
 		if (this.status === 0) {
 			return;
 		}
-		this.conn.end();
-		this.conn.destroy();
+		this.conn && this.conn.end();
+		this.conn && this.conn.destroy();
 		this.status = 0;
 	}
 
-	request(method, params) {
+	protected request(method: string, params: ElectrumRequestParams): Promise<any> {
 		if (this.status === 0) {
 			return Promise.reject(new Error('Connection to server lost, please retry'));
 		}
@@ -106,18 +119,18 @@ class Client {
 			const id = ++this.id;
 			const content = util.makeRequest(method, params, id);
 			this.callback_message_queue[id] = util.createPromiseResult(resolve, reject);
-			this.conn.write(content + '\n');
+			this.conn && this.conn.write(content + '\n');
 		});
 	}
 
-	requestBatch(method, params, secondParam) {
+	protected requestBatch(method: string, params: ElectrumRequestParams, secondParam?: ElectrumRequestBatchParams): Promise<any> {
 		if (this.status === 0) {
 			return Promise.reject(new Error('Connection to server lost, please retry'));
 		}
 		return new Promise((resolve, reject) => {
-			let arguments_far_calls = {};
-			let contents = [];
-			for (let param of params) {
+			const arguments_far_calls: Record<number, any> = {};
+			const contents = [];
+			for (const param of params) {
 				const id = ++this.id;
 				if (secondParam !== undefined) {
 					contents.push(util.makeRequest(method, [param, secondParam], id));
@@ -129,15 +142,15 @@ class Client {
 			const content = '[' + contents.join(',') + ']';
 			this.callback_message_queue[this.id] = util.createPromiseResultBatch(resolve, reject, arguments_far_calls);
 			// callback will exist only for max id
-			this.conn.write(content + '\n');
+			this.conn && this.conn.write(content + '\n');
 		});
 	}
 
-	response(msg) {
-		let callback;
+	private response(msg: any): void {
+		let callback: ((err: Error | null, result?: any) => void) | undefined;
 		if (!msg.id && msg[0] && msg[0].id) {
 			// this is a response from batch request
-			for (let m of msg) {
+			for (const m of msg) {
 				if (m.id && this.callback_message_queue[m.id]) {
 					callback = this.callback_message_queue[m.id];
 					delete this.callback_message_queue[m.id];
@@ -159,7 +172,8 @@ class Client {
 		}
 	}
 
-	onMessage(body, n) {
+	protected onMessage(body: string | undefined, n: number): void {
+		if (!body) return;
 		const msg = JSON.parse(body);
 		if (msg instanceof Array) {
 			this.response(msg);
@@ -172,27 +186,29 @@ class Client {
 		}
 	}
 
-	onConnect() {
+	protected onConnect(): void {
 	}
 
-	onClose(e) {
+	protected onClose(): void {
 		this.status = 0;
 
 		Object.keys(this.callback_message_queue).forEach(key => {
-			this.callback_message_queue[key](new Error('close connect'));
-			delete this.callback_message_queue[key];
+			const callback = this.callback_message_queue[Number(key)];
+			if (callback) {
+				callback(new Error('close connect'));
+			}
+			delete this.callback_message_queue[Number(key)];
 		});
 	}
 
-	onRecv(chunk) {
+	protected onRecv(chunk: Buffer): void {
 		this.mp.run(chunk);
 	}
 
-	onError(e) {
+	protected onError(e: Error): void {
 		if (this.onErrorCallback != null) {
 			this.onErrorCallback(e);
 		}
 	}
 }
 
-module.exports = Client;
